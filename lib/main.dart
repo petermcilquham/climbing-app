2621a5eb85855209da4b966320f5d55ca4025d0a
peter <peter.lm.schmidt@gmail.com>
2024-06-25 19:26:09 +0200

    removed unused dart import in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'src/app.dart';
 import 'profile.dart';
 import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
